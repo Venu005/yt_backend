2266b155c2e2f3aa84df9c0ec23fa3f6e448dbdd
Venu005 <venusaiyalamanchili@gmail.com>
2024-04-29 12:37:59 +0530

    cloudinary setup complete

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+const fileUpload = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const res = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //after uploading successfully
+    console.log("File uploaded successfully on cloudinary", res.url);
+    return res;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //removes temp file from server
+    console.log("Error in uploading file on cloudinary", error);
+    return null;
+  }
+};
+export { fileUpload };
